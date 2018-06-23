1e506c02d4a5947dfb8564317205bea411adba0f
Austen <austenmcd@gmail.com>
2018-06-22 20:56:29 -0400

    Adds first iteration of count display

--- src/Program.fs
+++ src/Program.fs
@@ -3,10 +3,13 @@
 open System
 open Terminal.Gui
 open NStack
+open Terminal.Gui
 
 let ustr (x:string) = ustring.Make(x)
 
 let stop = Action Application.RequestStop
+
+
 
 let quit() =
     if MessageBox.Query (50, 7, "Quit", "Are you sure you want to quit?", "Yes", "No") =0 then
@@ -18,23 +21,36 @@ let buildMenu() =
             [| MenuItem (ustr ("_Quit"), null, System.Action quit) 
              |])|])
 
-let increment (x : byref<_>) =
-    x <- x + 1
-    ()
-    
+type StateContainer() =
+    let mutable counter = 0
+    member this.IncCounter() =
+        counter <- counter + 1
+    member this.GetCounter() =
+        counter
 
-
+let numBox(st:StateContainer) =
+    let numStr = st.GetCounter().ToString()
+    MessageBox.Query(50, 7, "Count:", numStr, "ok")
+    |>ignore
 
 [<EntryPoint>]
 let main argv =
+
+    let state = new StateContainer()
+    let incState () =
+        state.IncCounter()
+
+    let DealWithButton() =
+        incState()
+        numBox(state)    
+
     Application.Init ()
-    let mutable num = 0
-    let button = new Button (ustr "Click me!", true, Clicked=System.Action (increment(&num)),  X=Pos.At(2), Y=Pos.At(1))
+    let button = new Button (ustr "Click me!", true, Clicked=System.Action (DealWithButton), X=Pos.At(2), Y=Pos.At(2))
     let top = Application.Top
     let win = new Window (ustr "Click Game", X=Pos.At(0), Y=Pos.At(1), Width=Dim.Fill(), Height=Dim.Fill())
     top.Add (buildMenu())
-    top.Add (button)
     top.Add (win)
+    top.Add (button)
     Application.Run ()
 
     0 // return an integer exit code
